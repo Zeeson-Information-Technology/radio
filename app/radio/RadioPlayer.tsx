5a12e755c130d8379d2fa93e2c2c858b5266a985
Zeeson <olabrazanislam@gmail.com>
2025-12-16 18:40:16 -0500

    update server

--- app/radio/RadioPlayer.tsx
+++ app/radio/RadioPlayer.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 interface LiveData {
   ok: boolean;
   isLive: boolean;
+  isMuted?: boolean;
   title: string | null;
   lecturer: string | null;
   startedAt: string | null;
